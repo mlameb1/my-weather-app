d55b172a27ee4ac53b448e867f512e0eac6fc126
mlameb1 <mlameb1@gmail.com>
2023-11-05 08:23:10 +0200

    Revert "removed the weather.js file and made some adjustments to the index.js file"
    This reverts commit fab0be2d1e2f2eedb69499528388178b793b7810.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Weather from "./Weather";
+import "./weather.css";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-    
-  </React.StrictMode>
+    <Weather />
+  </StrictMode>
 );
-
-reportWebVitals();
 
