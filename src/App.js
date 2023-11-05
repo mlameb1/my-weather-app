9c0d323128acda26667e1d34c2c9c08388bea62f
mlameb1 <mlameb1@gmail.com>
2023-11-05 08:25:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React from "react";
+
+
 import './App.css';
 
-export default function App() {
+function App() {
   return (
-  <div className = "App">
-        
-      
-        <a
-          className="Weather-App-link"
-          href="https://github.com/mlameb1/my-weather-app"
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+          Weather App
+         </h1>
+         <a
+          className="App-link"
+          href="https://github.com/mlameb1/my-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Coded by E Mlambo using open-sourced GitHub
+          E Mlambo Github link
         </a>
-    
+        
+      </header>
     </div>
+
+ 
   );
 }
+
+
+export default App;
